9a9716f355abb12743960959611900d96834e3ac
Gauthier_3090 <gauthier.pladet@gmail.com>
2022-01-24 16:26:32 +0100

    modify readme.md

--- env_script.py
+++
@@ -1,8 +0,0 @@
-import os
-import platform
-
-os_name = platform.system()
-if os_name == 'Windows':
-    os.system(
-        'python3 -m venv env && .\\env\\Scripts\\activate && pip install -r requirements.txt')
-    print('Create env... [OK]')
